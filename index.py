a5f06ad2909da14a0f22000e0c3c42cad695a9e1
Lindsay Anchors <lindsay.anchors@gmail.com>
2017-07-14 15:17:41 -0700

    Fix bug: extra indentation

--- index.py
+++ index.py
@@ -6,22 +6,22 @@ app = Flask(__name__)
 import json
 
 @app.route('/', methods=['GET'])
-  def index():
-    return '''
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Image Viewer</title>
-      </head>
-      <body>
-        <h1>Image Viewer</h1>
-        <form action="/img" method="post" enctype="multipart/form-data">
-          <input type="file" name="image" accept="image/*">
-          <input type="submit">
-        </form>
-      </body>
-      </html>
-    '''
+def index():
+  return '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Image Viewer</title>
+    </head>
+    <body>
+      <h1>Image Viewer</h1>
+      <form action="/img" method="post" enctype="multipart/form-data">
+        <input type="file" name="image" accept="image/*">
+        <input type="submit">
+      </form>
+    </body>
+    </html>
+  '''
