640c106c058ac86bccf3af0b3908b68e7376a51c
Lindsay Anchors <lindsay.anchors@gmail.com>
2017-07-15 20:43:51 -0700

    Saved repeated filename into a variable

--- test/test_index.py
+++ test/test_index.py
@@ -4,8 +4,9 @@ from index import app
 
 def test_img_post():
   "HTTP response data is the file sent with HTTP request"
+  test_img_filename = 'test/test_image'
   response = app.test_client().post('/img',
                           content_type='multipart/form-data',
-                          data={'image': open('test/test_image', 'rb')},
+                          data={'image': open(test_img_filename, 'rb')},
                           follow_redirects=True)
-  assert open('test/test_image', 'rb').read() == response.data
+  assert open(test_img_filename, 'rb').read() == response.data
