0d36baa670493e47752ea46a80e67f334cc31b1d
Lindsay Anchors <lindsay.anchors@gmail.com>
2017-07-19 17:11:19 -0700

    Save img in temp file object

--- imageviewer/index.py
+++ imageviewer/index.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, send_file, render_template
+from tempfile import TemporaryFile
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -7,6 +9,11 @@ def index():
 
 @app.route('/img', methods=['POST'])
 def imgSubmitted():
+  temp = TemporaryFile(suffix='.jpg')
   f = request.files['image']
-  f.save('imageviewer/uploaded_file')
-  return send_file('uploaded_file', mimetype=f.mimetype)
+  f.save(temp)
+  temp.seek(0)
+  response = send_file(temp, mimetype=f.mimetype)
+  f.close
+  temp.close
+  return response
